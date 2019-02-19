c1d5e0c09001625796fa89e2cd47f89a53ceecdb
= <=>
2019-02-19 13:12:17 +0300

    Added menu with contents

--- js/main.js
+++ js/main.js
@@ -6,15 +6,19 @@ const $tasks = $.cls('task');
 
 for (let $task of $tasks) {
 	let id = $task.id + '-marked';
+  let $contentsTask = $.id('contents-' + $task.id);
 	// check if there are marked tasks
-	if (localStorage.getItem(id) === 'on')
-		$task.classList.toggle('marked');
+	if (localStorage.getItem(id) === 'on') {
+    $task.classList.toggle('marked');
+    $contentsTask.classList.toggle('marked');
+  }
 	else localStorage.setItem(id, 'off');
 
 	// mark the task by right-clicking
   $task.on('contextmenu', function(e) {
     e.preventDefault();
     $task.classList.toggle('marked');
+    $contentsTask.classList.toggle('marked');
     if (localStorage.getItem(id) == 'on')
     	localStorage.setItem(id, 'off');
     else localStorage.setItem(id, 'on');
